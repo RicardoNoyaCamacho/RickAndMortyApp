fda1db90086a7b5c313fdc462e764da53c5bf793
Ricardo Noya <ricardo.noya@bykon.com.mx>
2022-02-12 13:28:24 -0600

    unit testing

--- src/app/locations/locations/locations.component.spec.ts
+++ src/app/locations/locations/locations.component.spec.ts
@@ -1,16 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { LocationsComponent } from './locations.component';
+import { Store, StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, Subscription } from 'rxjs';
+import { appReducers } from '../../store/app.reducers';
 
 describe('LocationsComponent', () => {
   let component: LocationsComponent;
   let fixture: ComponentFixture<LocationsComponent>;
 
+  let store: MockStore;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LocationsComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+        StoreModule.forRoot(appReducers),
+      ],
+      declarations: [LocationsComponent],
+      providers: [provideMockStore()],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
   });
 
   beforeEach(() => {
@@ -19,7 +34,44 @@ describe('LocationsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', inject([Store], (store: any) => {
+    expect(store).toBeTruthy();
+  }));
+
+  it('debe de llamarse el metodo list', () => {
+    const l = {
+      id: 0,
+      name: '',
+      type: '',
+      dimension: '',
+      residents: [''],
+      url: '',
+      created: new Date(),
+    };
+
+    const epsia = spyOn(component, 'list').and.callFake((resp) => of(resp));
+
+    component.list(l);
+
+    expect(epsia).toHaveBeenCalled();
+  });
+
+  it('unsubscribes cuando ejecute ngOnDestoy', () => {
+    component.locationsSubs = new Subscription();
+
+    spyOn(component.locationsSubs, 'unsubscribe');
+    component.ngOnDestroy();
+
+    expect(component.locationsSubs.unsubscribe).toHaveBeenCalledTimes(1);
+  });
+
+  it('ejecutar dispatch', () => {
+    let fixture = TestBed.createComponent(LocationsComponent);
+    let app = fixture.debugElement.componentInstance;
+
+    var mockStore = fixture.debugElement.injector.get(Store);
+    var storeSpy = spyOn(mockStore, 'dispatch').and.callFake(function () {
+      console.log('dispatching from the spy!');
+    });
   });
 });
